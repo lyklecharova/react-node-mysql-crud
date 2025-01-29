65fbb8986924e1d6ec65d52b982885533e346883
lyklecharova <lklecharova9@gmail.com>
2025-01-29 10:30:47 +0200

    Add 'Add' component for creating new books

--- client/vite-project/src/components/Add.jsx
+++ client/vite-project/src/components/Add.jsx
@@ -1,7 +1,63 @@
-const Add = () => {
-  return (
-    <div className=''>Add</div>
-  )
-}
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default Add
+const Add = () => {
+    const [book, setBook] = useState({
+        title: '',
+        desc: '',
+        price: null,
+        cover: '',
+    });
+    const navigate = useNavigate();
+    const handleChange = (e) => {
+        setBook((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+    const handleClick = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:8808/books', book);
+            navigate('/');
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <div className="form">
+            <h1>Add new book</h1>
+            <input
+                type="text"
+                placeholder="title"
+                onChange={handleChange}
+                name="title"
+            />
+            <input
+                type="text"
+                placeholder="desc"
+                onChange={handleChange}
+                name="desc"
+            />
+            <input
+                type="number"
+                placeholder="price"
+                onChange={handleChange}
+                name="price"
+            />
+            <input
+                type="text"
+                placeholder="cover"
+                onChange={handleChange}
+                name="cover"
+            />
+            <button className="formButton" onClick={handleClick}>
+                Add
+            </button>
+        </div>
+    );
+};
+
+export default Add;
