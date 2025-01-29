c0e1da934959f776b51eae57b3b327063d7a6d83
lyklecharova <lklecharova9@gmail.com>
2025-01-29 10:28:46 +0200

    Added Books component with API integration and delete functionality

--- client/vite-project/src/components/Books.jsx
+++ client/vite-project/src/components/Books.jsx
@@ -1,5 +1,59 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 const Books = () => {
-    return <div className="">Books</div>;
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        const fetchAllBooks = async () => {
+            try {
+                const res = await axios.get('http://localhost:8808/books');
+                setBooks(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchAllBooks();
+    }, []);
+    const handleDelete = async (id) => {
+        try {
+            const res = await axios.delete('http://localhost:8808/books/' + id);
+            window.location.reload();
+            setBooks(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    return (
+        <div className="">
+            <h1>Book Shop</h1>
+            <div className="books">
+                {books.map((book) => (
+                    <div className="book" key={book.id}>
+                        {book.cover && (
+                            <img src={book.cover} alt={book.title} />
+                        )}
+                        <h2>{book.title}</h2>
+                        <p>{book.desc}</p>
+                        <span>{book.price}</span>
+                        <button
+                            className="delete"
+                            onClick={() => handleDelete(book.id)}
+                        >
+                            Delete
+                        </button>
+                        <button className="update">
+                            <Link to={`/update/${book.id}`}>Update</Link>
+                        </button>
+                    </div>
+                ))}
+            </div>
+            <button>
+                <Link to="/add">Add new book</Link>
+            </button>
+        </div>
+    );
 };
 
 export default Books;
